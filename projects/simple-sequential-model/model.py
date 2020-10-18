d0c20c9f880ff0a2b6f8e83329478726f717c71a
Vincenzo <vincenzorm117@gmail.com>
2020-10-17 20:02:15 -0400

    backup

---
+++ projects/simple-sequential-model/model.py
@@ -0,0 +1,12 @@
+
+import tensorflow as tf
+from tensorflow import keras
+
+
+model = keras.Sequential()
+# Adds a densely-connected layer with 64 units to the model:
+model.add(keras.layers.Dense(64, activation='relu'))
+# Add another:
+model.add(keras.layers.Dense(64, activation='relu'))
+# Add a softmax layer with 10 output units:
+model.add(keras.layers.Dense(10, activation='softmax'))
